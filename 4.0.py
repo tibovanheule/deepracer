08e4d68cd5803c3c28eed9fddff4e69b97dc490e
Tibo Vanheule <mail@tibovanheule.space>
2023-09-18 14:56:14 +0200

    Update 4.0.py

--- 4.0.py
+++ 4.0.py
@@ -6,9 +6,11 @@ import math
 # Counterclockwise turn: d < 0
 # This is used to progmatically determine how fast we can go.
 def is_ccw(p0, p1, p2):
-    return ((p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1]));
+    return ((p1[0] - p0[0]) * (p2[1] - p0[1]) -  (p2[0] - p0[0]) * (p1[1] - p0[1]))
 
 def reward_function(params):
+
+    
     is_offtrack = params['is_offtrack']
     if is_offtrack:
         return 0.001
@@ -33,26 +35,27 @@ def reward_function(params):
     current_waypoint = closest_waypoints[1] - 1
     next_waypoint = closest_waypoints[1] % len(waypoints)
     next_next_waypoint = (closest_waypoints[1] + 1) % len(waypoints)
+    
     waypoint_first = waypoints[current_waypoint]
     waypoint_second = waypoints[next_waypoint]
     waypoint_third = waypoints[next_next_waypoint]
 
-    turn = is_ccw((waypoint_first[0],waypoint_first[1],),(waypoint_second[0],waypoint_second[1],),(waypoint_third[0],waypoint_third[1],))
+    turn = is_ccw(waypoint_first,waypoint_second,waypoint_third)
   
     if abs(turn) < 0.01:
-        # go fast
+        # go fast, next 3 waypoint are nearly on one line
         if speed == 3:
           reward += 14
         else:
           reward -= (5-speed)**2
     elif abs(turn) < 0.02:
-        # go medium
+        # go medium, next 3 waypoint are not exactly on one line, small turn
         if speed == 2:
           reward += 14
         else:
           reward -= 7
     else:
-        # go slow 
+        # go slow, next 3 waypoint are not on one line
         if speed == 1:
           reward += 14
         else:
